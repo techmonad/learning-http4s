b49a84a5e0255e71e86abf746c2f451b5180cbfb
Anand Singh <imanandksingh@gmail.com>
2019-03-15 22:54:03 +0530

    Init commit

---
+++ src/main/scala/com/techmonad/learning/http4s/Server.scala
@@ -0,0 +1,40 @@
+package com.techmonad.learning.http4s
+
+import cats.effect.{ConcurrentEffect, Effect, ExitCode, IO, IOApp, Timer, ContextShift}
+import cats.implicits._
+import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.client.blaze.BlazeClientBuilder
+import org.http4s.implicits._
+import fs2.Stream
+import scala.concurrent.ExecutionContext.global
+
+import org.http4s.server.middleware.Logger
+
+object Server {
+
+  def stream[F[_]: ConcurrentEffect](implicit T: Timer[F], C: ContextShift[F]): Stream[F, Nothing] = {
+    for {
+      client <- BlazeClientBuilder[F](global).stream
+      helloWorldAlg = HelloWorld.impl[F]
+      jokeAlg = Jokes.impl[F](client)
+
+      // Combine Service Routes into an HttpApp
+      // Can also be done via a Router if you
+      // want to extract a segments not checked
+      // in the underlying routes.
+      httpApp = (
+        Routes.helloWorldRoutes[F](helloWorldAlg) <+>
+        Routes.jokeRoutes[F](jokeAlg)
+      ).orNotFound
+
+      // With Middlewares in place
+      finalHttpApp = Logger(true, true)(httpApp)
+
+
+      exitCode <- BlazeServerBuilder[F]
+        .bindHttp(8080, "0.0.0.0")
+        .withHttpApp(finalHttpApp)
+        .serve
+    } yield exitCode
+  }.drain
+}
